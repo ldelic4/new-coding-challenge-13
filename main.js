c25a8035f636085c554750fc93908e13291b9c1a
Lea Delic <leadelic0429@gmail.com>
2024-11-03 16:38:47 -0500

    Fetch product data and display basic information

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 //task 1
+const productlist= document.getElementById('products');
+//Fetch product data and display basic information
+fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if(!response.ok) {
+            throw new Error ('no available product');
+
+        }
+        return response.json();
+    })
+    .then(products => {
+        products.forEach(product =>{
+            const listItem = document.createElement('li');
+            listItem.textContent= '${product.title} - $${product.price}'
+            productlist.appendChild(listItem);
+        
+        });
+    })
+    .catch(error => {
+        console.error('There was a problem.', error);
+
+    })
